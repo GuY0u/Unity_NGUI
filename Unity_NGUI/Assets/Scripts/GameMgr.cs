c1111465310223b4818792ef84189211382cfd14
GuYou <yysh123@gmail.com>
2020-07-17 16:21:37 +0900

    NGUI 마호마호 7장까지

---
+++ Unity_NGUI/Assets/Scripts/GameMgr.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour
+{
+
+    //배경 오브젝트
+    public GameObject _BgObj;
+
+    //추가배경(전등) 오브젝트
+    public GameObject _BgLightObj;
+
+    //배경의 이동속도
+    public float _moveSpeed;
+
+    //이동거리 체크
+    float _xPosMoveChk = 0f;
+    float _xLightPosMoveChk = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //매 시간 이동량 누적하여 저장
+        _xPosMoveChk += _moveSpeed * Time.deltaTime;
+        _xLightPosMoveChk += _moveSpeed * Time.deltaTime*0.5f;
+
+        //배경 이미지 왼쪽(x좌표)으로 매 프레임마다 이동
+        _BgObj.transform.localPosition += new Vector3(_moveSpeed * -1f * Time.deltaTime*0.5f, 0, 0);
+        _BgLightObj.transform.localPosition += new Vector3(_moveSpeed * -1f * Time.deltaTime, 0, 0);
+
+        //누적 이동량이 960보다 크면 체크(화면에서 배경이 나갈때)
+        if (_xPosMoveChk > 960.0f)
+        {
+            //누적량 초기화 -> 나중에 다시 나가게될시 체크해야되기 때문에
+            _xPosMoveChk = 0;
+
+            _BgLightObj.transform.localPosition = new Vector3(960 * -1.0f, 0, 0);
+        }
+        //누적 이동량이 960보다 크면 체크(화면에서 배경이 나갈때)
+        if (_xLightPosMoveChk > 960.0f)
+        {
+            //누적량 초기화 -> 나중에 다시 나가게될시 체크해야되기 때문에
+            _xLightPosMoveChk = 0;
+
+            _BgObj.transform.localPosition = new Vector3(960 * -1.0f, 0, 0);
+        }
+    }
+}
---
+++ Unity_NGUI/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //플레이어 이동속도
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, v, 0);
+        dir.Normalize();    
+        transform.Translate(dir.normalized * speed * Time.deltaTime);
+    }
+}
